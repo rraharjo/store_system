aa2f4b7d1629ec0ceab5d2a82e3cfce97967f570
Robert <raharjorobert2@gmail.com>
2024-09-13 23:37:16 -0400

    some files are not commited :)

--- database/init_database.sql
+++ database/init_database.sql
@@ -26,15 +26,15 @@ create table sellable(
 );
 
 create table purchase_transaction(
-    database_code    serial      primary key,
+    database_code    text      primary key,
     date_purchased   date        not null,
-    seller          varchar(100)
+    seller           varchar(100)
 );
 
 create table purchase_entry(
-    database_code    serial          primary key,
-    sellable_db_code  int             not null,
-    purchase_db_Code  int             not null,
+    database_code    text           primary key,
+    sellable_db_code  text             not null,
+    purchase_db_Code  text             not null,
     purchase_price   numeric(12, 2)    not null,
     qty             numeric(7)       not null,
     available_qty    numeric(7)       not null
@@ -49,14 +49,14 @@ add constraint fk_sellable foreign key (sellable_db_code)
     references sellable (database_code);
 
 create table selling_transaction(
-    database_code       serial          primary key,
+    database_code       text            primary key,
     transaction_date    date            not null      
 );
 
 create table selling_entry(
-    database_code       serial          primary key,
-    sellable_db_code    int,
-    selling_transaction_db_code int,
+    database_code       text            primary key,
+    sellable_db_code    text,
+    selling_transaction_db_code text,
     selling_price       numeric(12, 2),
     qty                 int
 );
@@ -70,14 +70,14 @@ add constraint fk_selling_transaction foreign key (selling_transaction_db_code)
     references selling_transaction (database_code);
 
 create table accounting_transaction(
-    database_code       serial          primary key,
+    database_code       text          primary key,
     transaction_name    varchar(50),
     transaction_date    date
 );
 
 create table accounting_transaction_entry(
-    database_code       serial          primary key,
-    at_db_code          int             not null,
+    database_code       text            primary key,
+    at_db_code          text            not null,
     debit               boolean         not null,
     amount              numeric(12, 2)  not null,
     t_account_number    varchar(25),
