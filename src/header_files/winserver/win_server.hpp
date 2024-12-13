6e14a60e08295611354bcab1b91f09a6e02d4f36
Robert <raharjorobert2@gmail.com>
2024-12-12 23:53:56 -0500

    add Win server structure

---
+++ src/header_files/winserver/win_server.hpp
@@ -0,0 +1,47 @@
+#include <winsock2.h>
+#include <ws2tcpip.h>
+#include <stdio.h>
+#include <thread>
+#include <mutex>
+#include <string>
+#include <iostream>
+#include <stdexcept>
+
+#ifndef WIN_SERVER
+#define WIN_SERVER
+
+#define DEFAULT_PORT "20000"
+#define LOOPBACK_ADDRESS "127.0.0.1"
+#define MAX_CLIENT 5
+namespace winnetwork
+{
+
+    class WinTCPServer
+    {
+    private:
+        const int max_client = MAX_CLIENT;
+
+        std::string port_number;
+        std::string ip_address;
+        int network_fam;
+        int socket_type;
+        int protocol;
+
+        WSAData wsa_data;
+        SOCKET listen_socket;
+
+        std::mutex my_mtx;
+
+        // shared resources
+        int num_of_client = 0;
+
+    private:
+        void init_socket();
+
+    public:
+        WinTCPServer(std::string ip_address, std::string port_number);
+
+        void start_server();
+    };
+}
+#endif
---
+++ src/source_files/winserver/win_server.cpp
@@ -0,0 +1,64 @@
+#include "winserver/win_server.hpp"
+
+//TODO: Declare a thread handler here
+
+winnetwork::WinTCPServer::WinTCPServer(std::string ip_address, std::string port_number) : ip_address(ip_address), port_number(port_number)
+{
+    this->network_fam = AF_INET;
+    this->socket_type = SOCK_STREAM;
+    this->protocol = IPPROTO_TCP;
+}
+
+void winnetwork::WinTCPServer::init_socket()
+{
+    int i_result;
+    i_result = WSAStartup(MAKEWORD(2, 2), &this->wsa_data);
+    if (i_result != 0)
+    {
+        throw std::runtime_error("Error on WSAStartup() with error code " + std::to_string(i_result) + "\n");
+    }
+
+    struct addrinfo hint, *addrinfo_result;
+    memset(&hint, 0, sizeof(hint));
+    hint.ai_family = this->network_fam;
+    hint.ai_protocol = this->protocol;
+    hint.ai_socktype = this->socket_type;
+    i_result = getaddrinfo(this->ip_address.c_str(), this->port_number.c_str(), &hint, &addrinfo_result);
+    if (i_result != 0)
+    {
+        WSACleanup();
+        throw std::runtime_error("Error on getaddrinfo() with error code " + std::to_string(i_result) + "\n");
+    }
+
+    this->listen_socket = socket(addrinfo_result->ai_family, addrinfo_result->ai_socktype, addrinfo_result->ai_protocol);
+    if (this->listen_socket == INVALID_SOCKET)
+    {
+        int error_code = WSAGetLastError();
+        WSACleanup();
+        throw std::runtime_error("Error on socket() with error code " + std::to_string(error_code) + "\n");
+    }
+
+    i_result = bind(this->listen_socket, addrinfo_result->ai_addr, sizeof(*addrinfo_result));
+    if (i_result == SOCKET_ERROR)
+    {
+        int error_code = WSAGetLastError();
+        WSACleanup();
+        freeaddrinfo(addrinfo_result);
+        throw std::runtime_error("Error on bind() with error code " + std::to_string(error_code) + "\n");
+    }
+    freeaddrinfo(addrinfo_result);
+
+    i_result = listen(this->listen_socket, SOMAXCONN);
+    if (i_result == SOCKET_ERROR)
+    {
+        int error_code = WSAGetLastError();
+        WSACleanup();
+        throw std::runtime_error("Error on listen() with error code " + std::to_string(error_code) + "\n");
+    }
+    std::cout << "listening at port " << this->port_number << std::endl;
+}
+
+void winnetwork::WinTCPServer::start_server(){
+    this->init_socket();
+    //TODO: Handle thread
+}
